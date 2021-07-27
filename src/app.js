7d287d6c9bcab130536acafe53b70724b0d94492
Travis Byrman <travis-byrman@live.nmit.ac.nz>
2021-07-28 09:28:26 +1200

    Saved

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
+const test = require('./test')
+
 const fs = require('fs')
 
 fs.writeFileSync('mynotes.text', 'Hello world!')
 
 fs.appendFileSync('mynotes.text', '\nHello again!')
+
+test()
---
+++ src/test.js
@@ -0,0 +1,7 @@
+const test = function () {
+    console.log('Hello from test file')
+}
+
+//test()
+
+module.exports = test
