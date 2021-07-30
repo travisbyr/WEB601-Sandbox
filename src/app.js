8808c3d5474ea5b69ed2dfbd0f43f21e7a9ed225
Travis Byrman <travis-byrman@live.nmit.ac.nz>
2021-07-30 13:13:35 +1200

    Saved

---
+++ src/app.js
@@ -0,0 +1,14 @@
+// console.log(process.pid);
+// console.log(process.argv);
+// console.log(process.pid);
+// console.log(process.argv);
+// console.log(process.env);
+// console.log(process.platform);
+// console.log(process.release);
+// console.log(process.versions);
+// console.log(process.stdin);
+// console.log(process.stdout);
+// process.uptime();
+// process.memoryUsage();
+// process.exit();
+// process.kill());
